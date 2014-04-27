828cb7f6692289635339e9c9d0a417e9466b2e93
Csaba Molnar <elodin@innocent.com>
2014-04-27 14:34:07 +0200

    First racket prototype - untested

---
+++ racket.cpp
@@ -0,0 +1,42 @@
+#include "racket.hpp"
+
+void Racket::step()
+{
+    double dist = (m_target - m_position).getLength;
+    if (dist > 0.01)
+    {
+        m_position += m_velocity;
+        if ((m_target - m_position).getLength < dist)
+        {
+            m_position = m_target;
+            m_velocity = vector2df(0, 0);
+        }
+    }
+}
+
+Racket::Racket(vector2df pos, vector2df size, vector3df map_size)
+{
+    m_speed = 1;
+    m_position = pos;
+    m_size     = size;
+    map_size   = map_size;
+}
+
+void Racket::setTarget(const vector2df& target)
+{
+    m_target = target;
+    
+    double i = fabs(m_position.X - m_target.X);
+    double j = fabs(m_position.Y - m_target.Y);
+
+    m_velocity.Y = m_speed / sqrt(i*i / j*j + 1);
+    m_velocity.X = i / j * m_velocity.Y;
+}
+
+void Racket::animate(int dt)
+{
+    for (int i = 0; i < dt; i++)
+    {
+        step();
+    }
+}
---
+++ racket.hpp
@@ -0,0 +1,36 @@
+#ifndef RACKET_HPP
+#define RACKET_HPP
+
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+
+class Racket
+{
+private:
+
+    ISceneNode*     m_node;
+
+    double          m_speed;
+    vector2df       m_size;
+    vector2df       m_position;
+    vector2df       m_velocity;
+    vector2df       m_target;
+
+    void step();
+
+public:
+    Racket(vector2df pos, vector2df size, vector3df map_size);
+
+    void setPosition(const vector2df& pos)              { m_position = pos;  }
+    const vector2df& getSize() const                    { return m_size;     }
+    const vector2df& getPosition() const                { return m_position; }
+    const vector2df& getVelocity() const                { return m_velocity; }
+
+    void setTarget(const vector2df& target);
+    void animate(int dt);
+};
+
+#endif
